86dda356ba322853ca55dccfcb458a6bd093a075
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-09-11 21:21:54 +0700

    Create mock data

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,50 @@ def greeting():
 def shark():
   return 'Shark'
 
+## Game list, we will use DynamoDB
+
+GAMES = [
+  {
+    'title': 'Bida',
+    'genre': ' sports',
+    'played': False
+  },
+  {
+    'title': 'Bida',
+    'genre': ' sports',
+    'played': False
+  },
+  {   
+    'title':'Evil Within',
+    'genre':'horror',
+    'played': False,
+  },
+  {   
+    'title':'The last of us',
+    'genre':'survival',
+    'played': True,
+  },
+  {  
+    'title':'Days gone',
+    'genre':'horror/survival',
+    'played': False,
+  },
+  {   
+    'title':'Mario',
+    'genre':'retro',
+    'played': True,
+  }
+]
+
+# The GET route handler
+@app.route('/games', methods=['GET'])
+def all_games():
+  return jsonify({
+    'games': GAMES,
+    'status': 'success',
+
+  })
+
 
 if __name__ == '__main__':
   app.run(debug=True)
